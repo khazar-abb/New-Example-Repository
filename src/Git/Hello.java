d375f62a0972c40e2e18bf84b9956264faca5366
khazarbaghirov <khazar.baghirov@abb-bank.az>
2026-03-03 07:43:17 +0400

    test

--- src/Git/Hello.java
+++ src/Git/Hello.java
@@ -2,9 +2,8 @@ package Git;
 
 public class Hello {
     public static void main(String[] args) {
-
-        System.out.println("Salam GitHub!");
         System.out.println("Salam GitHub!");
 
+        System.out.println("Do we have changes?");
     }
 }
